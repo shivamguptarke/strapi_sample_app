5357067d4e43b6c3944d6100b50fd87364c73c04
shivamguptarke <shivamguptarke@gmail.com>
2022-06-17 17:05:24 +0530

    complete

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "firstDB"),
+      user: env("DATABASE_USER", "postgres"),
+      password: env("DATABASE_PASSWORD", "SHIVAM"),
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+  }});
